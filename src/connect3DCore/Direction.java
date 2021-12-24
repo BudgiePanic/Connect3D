0cbb2b234982bf5e2e40dbf787f5188d1be17d8f
Ben Sanson <bsanson90@gmail.com>
2021-12-24 17:31:12 +1300

    added direction constants

--- src/connect3DCore/Direction.java
+++ src/connect3DCore/Direction.java
@@ -2,6 +2,7 @@ package connect3DCore;
 /**
  * A direction of travel through the board.
  * There are 26 directions you can travel in, from a given point in the 3D space.
+ * These 26 directions have been captured as the following constants below plus and identity that represents no direction.
  * @author Benjamin
  *
  */
@@ -39,4 +40,24 @@ interface Direction {
 	Direction LeftANDdown = (t) -> new Tuple(t.x - 1, t.y - 1, t.z);
 	
 	//Towards AND away Height diagonals
+	Direction TowardsANDdown = (t) -> new Tuple(t.x, t.y - 1, t.z - 1);
+	Direction TowardsANDup = (t) -> new Tuple(t.x, t.y + 1, t.z - 1);
+	Direction AwayANDdown = (t) -> new Tuple(t.x, t.y - 1, t.z + 1);
+	Direction AwayANDup = (t) -> new Tuple(t.x, t.y + 1, t.z + 1);
+	
+	//Towards AND Left AND Height
+	Direction LeftANDtowardsANDdown = (t) -> new Tuple(t.x - 1, t.y - 1, t.z - 1);
+	Direction LeftANDtowardsANDup = (t) -> new Tuple(t.x - 1, t.y + 1, t.z - 1);
+	
+	//Towards AND Right AND height
+	Direction RightANDtowardsANDup = (t) -> new Tuple(t.x + 1, t.y + 1, t.z - 1);
+	Direction RightANDtowardsANDdown = (t) -> new Tuple(t.x + 1, t.y - 1, t.z - 1);
+	
+	//Away AND Left AND Height
+	Direction LeftANDawayANDup = (t) -> new Tuple(t.x - 1, t.y + 1, t.z + 1);
+	Direction LeftANDawayANDdown = (t) -> new Tuple(t.x - 1, t.y - 1, t.z + 1);
+	
+	//Away AND Right AND Height
+	Direction RightANDawayANDup = (t) -> new Tuple(t.x + 1, t.y + 1, t.z + 1);
+	Direction RightANDawayANDdown = (t) -> new Tuple(t.x + 1, t.y - 1, t.z + 1);
 }
