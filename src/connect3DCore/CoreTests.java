2ef4a1452992efeee6881a7c067e5b7776a8dc93
Ben Sanson <bsanson90@gmail.com>
2021-12-24 20:26:49 +1300

    added new tests

--- src/connect3DCore/CoreTests.java
+++ src/connect3DCore/CoreTests.java
@@ -29,22 +29,14 @@ class CoreTests {
 	
 	@Test
 	void testFullBoard() {
-		Board b = makeTestBoard(4);
-		for(int x = 0; x < 4; x++) {
-			for(int y = 0; y < 4; y++) {
-				for(int z = 0; z < 4; z++) {
-					b.placePieceAt(x, y, z, Piece.RED);
-				}
-			}
-		}
-		assertTrue(b.isBoardFull());
+		fail("Test not implemented yet!");
 	}
 	
 	@Test
 	void testpartialBoard() {
 		Board b = makeTestBoard(4);
-		for(int x = 0, y = 0, z = 0; x < 4; x++) {
-			b.placePieceAt(x, y, z, Piece.RED);
+		for(int x = 0, z = 0; x < 4; x++) {
+			b.placePieceAt(x, z, Piece.RED);
 		}
 		assertFalse(b.isBoardFull());
 	}
@@ -59,10 +51,85 @@ class CoreTests {
 	void testSimpleWinHorizon() {
 		Board b = makeTestBoard(4);
 		for(int i = 0; i < 4; i++) {
-			b.placePieceAt(i, 0, 0, Piece.RED);
+			b.placePieceAt(i, 0, Piece.RED);
 		}
 		assertTrue(b.hasSomeoneWon());
 		assertTrue(b.getWinner() == Piece.RED);
+		assertTrue(b.getWinningPieceLocations().isEmpty() == false);
+		System.out.println(b.getWinningPieceLocations());
+	}
+	
+	@Test
+	void testSimpleWinVertical() {
+		Board b = makeTestBoard(4);
+		for(int i = 0; i < 4; i++) {
+			b.placePieceAt(0, 0, Piece.RED);
+		}
+		assertTrue(b.hasSomeoneWon());
+		assertTrue(b.getWinner() == Piece.RED);
+		assertTrue(b.getWinningPieceLocations().isEmpty() == false);
+		System.out.println(b.getWinningPieceLocations());
+	}
+	@Test
+	void testSimpleWinDiagonal() {
+		Board b = makeTestBoard(4);
+		for(int i = 0; i < 4; i++) {
+			b.placePieceAt(i, i, Piece.RED);
+		}
+		assertTrue(b.hasSomeoneWon());
+		assertTrue(b.getWinner() == Piece.RED);
+		assertTrue(b.getWinningPieceLocations().isEmpty() == false);
+		System.out.println(b.getWinningPieceLocations());
+	}
+	@Test
+	void testSimpleWinDepth() {
+		Board b = makeTestBoard(4);
+		for(int i = 0; i < 4; i++) {
+			b.placePieceAt(0, i, Piece.RED);
+		}
+		assertTrue(b.hasSomeoneWon());
+		assertTrue(b.getWinner() == Piece.RED);
+		assertTrue(b.getWinningPieceLocations().isEmpty() == false);
+		System.out.println(b.getWinningPieceLocations());
+	}
+	
+	void testWinTwoColors() {
+		Board b = makeTestBoard(4);
+		
+		b.placePieceAt(0, 0, Piece.RED);
+		b.placePieceAt(0, 1, Piece.BLUE);
+		
+		b.placePieceAt(1, 0, Piece.RED);
+		b.placePieceAt(0, 2, Piece.BLUE);
+		
+		b.placePieceAt(2, 0, Piece.RED);
+		b.placePieceAt(0, 3, Piece.BLUE);
+		
+		b.placePieceAt(3, 0, Piece.RED);
+		
+		assertTrue(b.hasSomeoneWon());
+		assertTrue(b.getWinner() == Piece.RED);
+		assertTrue(b.getWinningPieceLocations().isEmpty() == false);
+		System.out.println(b.getWinningPieceLocations());
+	}
+	
+	void testWinSecondPlayer() {
+		Board b = makeTestBoard(4);
+		
+		b.placePieceAt(0, 0, Piece.RED);
+		b.placePieceAt(3, 0, Piece.BLUE);
+		
+		b.placePieceAt(1, 0, Piece.RED);
+		b.placePieceAt(2, 1, Piece.BLUE);
+		
+		b.placePieceAt(2, 0, Piece.RED);
+		b.placePieceAt(1, 2, Piece.BLUE);
+		
+		b.placePieceAt(0, 0, Piece.RED);
+		b.placePieceAt(0, 3, Piece.BLUE);
+		
+		assertTrue(b.hasSomeoneWon());
+		assertTrue(b.getWinner() == Piece.BLUE);
 		assertTrue(b.getWinningPieceLocations().isEmpty() == false);
 		System.out.println(b.getWinningPieceLocations());
 	}
