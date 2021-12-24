a2e49768cb0e71685f8d0d74da2f5fb02bbf870e
Ben Sanson <bsanson90@gmail.com>
2021-12-24 19:55:56 +1300

    Added useful toString to tuple

--- src/connect3DCore/Tuple.java
+++ src/connect3DCore/Tuple.java
@@ -29,4 +29,9 @@ public class Tuple {
 	public Tuple(int x, int y, int z) {
 		this.x = x; this.y = y; this.z = z;
 	}
+	
+	@Override
+	public String toString() {
+		return x + " " + y + " " + z;
+	}
 }
