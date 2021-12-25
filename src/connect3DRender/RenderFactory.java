d9f657d0a8b9a820363d3caeb1ceb2f9a6af8358
Ben Sanson <bsanson90@gmail.com>
2021-12-25 14:49:08 +1300

    Supply text renderers

--- src/connect3DRender/RenderFactory.java
+++ src/connect3DRender/RenderFactory.java
@@ -3,7 +3,8 @@ package connect3DRender;
 public final class RenderFactory {
 	
 	public static Renderer Renderer(String type) throws IllegalArgumentException {
-		//TODO
-		return null;
+		//currently only supports the text renderer
+		if(type.toLowerCase() == "text") return new TextRenderer();
+		throw new IllegalArgumentException("Unknown renderer type! ->"+type);
 	}
 }
