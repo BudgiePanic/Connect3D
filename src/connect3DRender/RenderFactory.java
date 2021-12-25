efcb5a46b2594116d0147b568341d46b482168dd
Ben Sanson <bsanson90@gmail.com>
2021-12-25 16:15:19 +1300

    render factory now provides text renderers

--- src/connect3DRender/RenderFactory.java
+++ src/connect3DRender/RenderFactory.java
@@ -1,7 +1,21 @@
 package connect3DRender;
 
+/**
+ * Renderer factory is the supplier of renderer objects to the rest of the program.
+ * @author Benjamin
+ *
+ */
 public final class RenderFactory {
 	
+	/**
+	 * Create a new type of renderer. Renderer will be uninitialized. 
+	 * @param type
+	 *  The type of renderer that is desired.
+	 * @return
+	 *  The renderer object.
+	 * @throws IllegalArgumentException
+	 *  Thrown if the renderer type does not exist.
+	 */
 	public static Renderer Renderer(String type) throws IllegalArgumentException {
 		//currently only supports the text renderer
 		if(type.toLowerCase() == "text") return new TextRenderer();
