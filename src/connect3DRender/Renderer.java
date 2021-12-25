89b6e72239b9558f17c85d9e0478642eb207a0e5
Ben Sanson <bsanson90@gmail.com>
2021-12-25 14:48:49 +1300

    Describes the operations the renderer needs to be able to perform for IO

--- src/connect3DRender/Renderer.java
+++ src/connect3DRender/Renderer.java
@@ -1,5 +1,51 @@
 package connect3DRender;
 
-public interface Renderer {
-
+/**
+ * Describes the operations a renderer needs to be able to perform.
+ * Renderer will redraw all the components registered to it on the redraw call.
+ * @author Benjamin
+ *
+ */
+public interface Renderer extends Graphics, Subject {
+	
+	/**
+	 * Initialize the renderer.
+	 * @throws InitializationException
+	 *  Thrown if the renderer could not initialize itself. 
+	 */
+	public void initialize() throws InitializationException;
+	
+	/**
+	 * Respond to any user input this frame.
+	 * Notify observers of relevant events. 
+	 * Such as updating the camera location in response to a mouse drag
+	 * or notifying of a mouse click.
+	 * @throws IllegalStateException
+	 *  Thrown if renderer is not initialized. 
+	 */
+	public void pollEvents() throws IllegalStateException;
+	
+	/**
+	 * Redraw all the components that have been registered to this renderer.
+	 * The specifics of how the redrawing is left to the implementation.
+	 * @throws IllegalStateException
+	 *  Thrown if the renderer has not been initialized.
+	 */
+	public void redraw() throws IllegalStateException;
+	
+	/**
+	 * Add a new component for the renderer to redraw on the redraw method.
+	 * @param c
+	 *  The new drawable component.
+	 */
+	public void addComponent(Component c);
+	
+	/**
+	 * Remove a component so the renderer does not draw it anymore.
+	 * @param c
+	 *  The component to be removed.
+	 * @return true if the component was removed.
+	 */
+	public boolean removeComponent(Component c);
+	
 }
