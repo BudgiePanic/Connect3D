c440c402cca4b3cbdd49f17537d05d50af1ef5a1
Ben Sanson <bsanson90@gmail.com>
2021-12-27 13:36:08 +1300

    make a test suite to make sure the renderer concepts work

---
+++ src/connect3DRender/TextRenderingTests.java
@@ -0,0 +1,18 @@
+package connect3DRender;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/**
+ * Tests to make sure the text rendering ideas will work.
+ * @author Benjamin
+ *
+ */
+class TextRenderingTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
