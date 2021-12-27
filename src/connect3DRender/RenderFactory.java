abdd49ba43dde0c4a06d9e42b7fffe74cbbf65d2
Ben Sanson <bsanson90@gmail.com>
2021-12-27 16:23:41 +1300

    updated renderer contract to pass board dimension along to the renderer
    The text renderer needs this information to convert from game space to screen space and I don't know how else to give the renderer this information

--- src/connect3DRender/RenderFactory.java
+++ src/connect3DRender/RenderFactory.java
@@ -11,14 +11,17 @@ public final class RenderFactory {
 	 * Create a new type of renderer. Renderer will be uninitialized. 
 	 * @param type
 	 *  The type of renderer that is desired.
+	 * @param boardDimension
+	 *  The length, width, height of the board. Kind of naughty
+	 *  but I don't know how else to get this information into the renderer.
 	 * @return
 	 *  The renderer object.
 	 * @throws IllegalArgumentException
 	 *  Thrown if the renderer type does not exist.
 	 */
-	public static Renderer Renderer(String type) throws IllegalArgumentException {
+	public static Renderer Renderer(String type, int boardDimension) throws IllegalArgumentException {
 		//currently only supports the text renderer
-		if(type.toLowerCase() == "text") return new TextRenderer();
+		if(type.toLowerCase() == "text") return new TextRenderer(boardDimension);
 		throw new IllegalArgumentException("Unknown renderer type! ->"+type);
 	}
 }
