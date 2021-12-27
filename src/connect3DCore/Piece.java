86676e33f3a4f9d2d03d8de4b648e2652dfde591
Ben Sanson <bsanson90@gmail.com>
2021-12-27 15:41:26 +1300

    pieces can now produce a color character

--- src/connect3DCore/Piece.java
+++ src/connect3DCore/Piece.java
@@ -6,12 +6,43 @@ package connect3DCore;
  *
  */
 public enum Piece {
-	GREEN,
-	WHITE,
-	BLUE,
-	RED,
-	ORANGE,
-	PURPLE,
-	YELLOW,
-	EMPTY
+	GREEN {
+		@Override
+		public char charRep() { return 'G'; }
+	},
+	WHITE {
+		@Override
+		public char charRep() { return 'W'; }
+	},
+	BLUE {
+		@Override
+		public char charRep() { return 'B'; }
+	},
+	RED {
+		@Override
+		public char charRep() { return 'R'; }
+	},
+	ORANGE {
+		@Override
+		public char charRep() { return 'O'; }
+	},
+	PURPLE {
+		@Override
+		public char charRep() { return 'P'; }
+	},
+	YELLOW {
+		@Override
+		public char charRep() { return 'Y'; }
+	},
+	EMPTY {
+		@Override
+		public char charRep() { return 'X'; }
+	};
+	
+	/**
+	 * Returns a char that represents this color.
+	 * @return
+	 *  A char that represents this Piece's color.
+	 */
+	public abstract char charRep();
 }
