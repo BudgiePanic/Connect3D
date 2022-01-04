2bf4bb60626b297a84ffe5a6b687a1c4b2bb1a49
Ben Sanson <bsanson90@gmail.com>
2022-01-04 18:58:26 +1300

    Create MathUtilTests.java

---
+++ src/connect3DUtil/MathUtilTests.java
@@ -0,0 +1,19 @@
+package connect3DUtil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the functionality of the math utility methods
+ * @author Benjamin
+ *
+ */
+class MathUtilTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
