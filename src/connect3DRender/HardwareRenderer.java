05ba7e5dc9fa1b1abec84807f191d3cf39c939d0
Benjamin Sanson <bsanson90@gmail.com>
2022-01-18 23:41:29 +1300

    Create HardwareRenderer.java

---
+++ src/connect3DRender/HardwareRenderer.java
@@ -0,0 +1,91 @@
+package connect3DRender;
+
+import connect3DCore.Piece;
+
+public final class HardwareRenderer implements Renderer {
+
+	@Override
+	public void drawCylinderAt(int x, int y, int z, float radius, float height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawCubeAt(int x, int y, int z, float width, float height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawSphereAt(int x, int y, int z, float radius) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawMessage(String msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setActiveColor(Piece p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notifyObservers() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initialize() throws InitializationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pollEvents() throws IllegalStateException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void redraw() throws IllegalStateException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addComponent(Component c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean removeComponent(Component c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
