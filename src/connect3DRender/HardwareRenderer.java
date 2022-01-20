5cb40f31dcf87d2a01b33b33ef104342452170f1
Benjamin Sanson <bsanson90@gmail.com>
2022-01-20 15:02:56 +1300

    Started creating HW renderer.

--- src/connect3DRender/HardwareRenderer.java
+++ src/connect3DRender/HardwareRenderer.java
@@ -1,91 +1,145 @@
 package connect3DRender;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import connect3DCore.Piece;
 
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+/**
+ * Hardware renderer uses the capabilities of LWJGL to display the game via OpenGL.
+ * If you do not call the poll events method of the renderer, it will block and eventually crash.
+ * Presumably this is because the event queue buffer fills up with unconsumed events.
+ * @see "https://www.lwjgl.org/guide"
+ * @see "https://lwjglgamedev.gitbooks.io/3d-game-development-with-lwjgl/content/"
+ * @author Benjamin
+ *
+ */
 public final class HardwareRenderer implements Renderer {
+	
+	/**
+	 * List of components this renderer is currently drawing.
+	 */
+	private final List<Component> drawables = new ArrayList<Component>();
+	
+	/**
+	 * List of observers interested in receiving updates from this renderer.
+	 */
+	private final List<Observer> observers = new ArrayList<Observer>();
+	
+	private volatile int x,y,z;
+	private volatile String message;
+//========================================
+	/**
+	 * The handle of the glfw window.
+	 */
+	private long a_window; 
+	
+	/**
+	 * The width of the GLFW window in pixels
+	 */
+	private int WIDTH = 1280;
+	
+	/**
+	 * The height of the GLFW window in pixels.
+	 */
+	private int HEIGHT = 720;
+	
+	/**
+	 * remember if the renderer has been initialized.
+	 */
+	private boolean initialized = false;
+	
+	@Override
+	public void addComponent(Component c) { this.drawables.add(c); }
 
 	@Override
-	public void drawCylinderAt(int x, int y, int z, float radius, float height) {
-		// TODO Auto-generated method stub
-
-	}
+	public boolean removeComponent(Component c) { return this.drawables.remove(c); }
+	
+	@Override
+	public void addObserver(Observer o) { this.observers.add(o); }
 
 	@Override
-	public void drawCubeAt(int x, int y, int z, float width, float height) {
-		// TODO Auto-generated method stub
-
-	}
+	public void removeObserver(Observer o) { this.observers.remove(o); }
 
 	@Override
-	public void drawSphereAt(int x, int y, int z, float radius) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void drawMessage(String msg) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setActiveColor(Piece p) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void addObserver(Observer o) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void removeObserver(Observer o) {
-		// TODO Auto-generated method stub
-
-	}
+	public void setActiveColor(Piece p) {}
 
 	@Override
 	public void notifyObservers() {
-		// TODO Auto-generated method stub
-
+		this.observers.forEach((o)->{o.update(x, y, z, message);});
 	}
 
 	@Override
 	public void initialize() throws InitializationException {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello LWJGL " + Version.getVersion() + "! inside hardware renderer");
+		GLFWErrorCallback.createPrint(System.err).set();
+		if(!glfwInit()) throw new InitializationException("GLFW library failed to initialize!");
+		//set some stuff up.
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		//try to make the window.
+		a_window = glfwCreateWindow(WIDTH, HEIGHT, "Connect3D", NULL, NULL);
+		if(a_window == NULL) throw new InitializationException("GLFW window creation failed!");
+		
+		glfwMakeContextCurrent(a_window);
+		glfwSwapInterval(1); //enable V-Sync
+		glfwShowWindow(a_window);
+		
+		GL.createCapabilities();
+		glClearColor(15.0f, 100.0f, 82.0f, 255.0f);
+		
+		this.initialized = true;
+		System.out.println("init HW renderer complete");
 	}
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-
+		glfwFreeCallbacks(a_window);
+		glfwDestroyWindow(a_window);
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+		System.out.println("destroyed HW renderer");
+	}
+	
+	@Override
+	public boolean isActive() {
+		if(!initialized) throw new IllegalStateException("uninitialized renderer cannot be active");
+		return !glfwWindowShouldClose(a_window);
 	}
 
 	@Override
 	public void pollEvents() throws IllegalStateException {
-		// TODO Auto-generated method stub
-
+		System.out.println("events polled");
+		glfwPollEvents();
 	}
 
 	@Override
 	public void redraw() throws IllegalStateException {
-		// TODO Auto-generated method stub
-
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glfwSwapBuffers(a_window);
+		System.out.println("redrawn");
 	}
+	
+	@Override
+	public void drawCylinderAt(int x, int y, int z, float radius, float height) {}
 
 	@Override
-	public void addComponent(Component c) {
-		// TODO Auto-generated method stub
-
-	}
+	public void drawCubeAt(int x, int y, int z, float width, float height) {}
 
 	@Override
-	public boolean removeComponent(Component c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public void drawSphereAt(int x, int y, int z, float radius) {}
+
+	@Override
+	public void drawMessage(String msg) {}
 
 }
