940f07c8b25e2b9ad54edd6a60d435fbba6382ea
Benjamin Sanson <bsanson90@gmail.com>
2022-01-25 17:50:33 +1300

    Added texture loading with stb lib
    I don't know if this will work from a jar

--- src/connect3DResources/FileLoader.java
+++ src/connect3DResources/FileLoader.java
@@ -3,6 +3,13 @@ package connect3DResources;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.system.MemoryStack;
+import static org.lwjgl.stb.STBImage.*;
+
+import connect3DUtil.Texture;
 
 /**
  * Helper class the loads stuff from files.
@@ -39,6 +46,35 @@ public final class FileLoader {
 			e.printStackTrace();
 			throw new Exception(e);
 		}
+		return answer;
+	}
+	
+	/**
+	 * Load the RGBA information from an image using the stb library.
+	 * @see "https://github.com/lwjglgamedev/lwjglbook/blob/master/chapter08/src/main/java/org/lwjglb/engine/graph/Texture.java"
+	 * @param relativePath
+	 *  The relative location of the image file from the FileLoader location
+	 * @return
+	 *  An object holding all the needed texture information.
+	 * @throws Exception
+	 *  Thrown if no image is found at the specified location
+	 */
+	public static Texture loadAndCreateTexture(String relativePath) throws Exception{
+		ByteBuffer buffer;
+		int width, height;
+		try(MemoryStack stackFrame = MemoryStack.stackPush()){
+			IntBuffer w = stackFrame.mallocInt(1);
+			IntBuffer h = stackFrame.mallocInt(1);
+			IntBuffer channels = stackFrame.mallocInt(1);
+			
+			buffer = stbi_load(relativePath, w,h,channels, 4);
+			if(buffer == null) {
+				throw new Exception("Image: ["+relativePath+"] was not loaded! >>"+stbi_failure_reason());
+			}
+			width = w.get(); height = h.get();
+		}
+		Texture answer = new Texture(width, height, buffer);
+		stbi_image_free(buffer);
 		return answer;
 	}
 }
