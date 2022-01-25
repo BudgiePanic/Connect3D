e2ea51d0a9f5aac4289ae59c8e6b84291bd6a6bd
Benjamin Sanson <bsanson90@gmail.com>
2022-01-25 18:11:53 +1300

    Update fragment.fs
    added a documenting comment

--- src/connect3DResources/fragment.fs
+++ src/connect3DResources/fragment.fs
@@ -3,7 +3,7 @@
 in vec2 exTextureCoordinate;
 out vec4 fragmentColor;
 
-uniform sampler2D texture_sampler;
+uniform sampler2D texture_sampler; //sampler2D holds the texture unit on the GPU that should be queried for texture color data.
 
 void main()
 {
