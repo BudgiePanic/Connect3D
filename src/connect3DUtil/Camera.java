6b88a9d9e7ac030f4bf7dd3c6c281438aabb1498
Benjamin Sanson <bsanson90@gmail.com>
2022-02-02 14:33:20 +1300

    Fixed camera woes
    Still need to translate the position of draws to centre them for the camera

--- src/connect3DUtil/Camera.java
+++ src/connect3DUtil/Camera.java
@@ -39,14 +39,16 @@ public class Camera {
 	public float radius = 2.0f;
 	
 	/**
-	 * The XZ displacement of the camera from 0 degrees in radians.
+	 * The displacement of the camera in radians along the polar (Z) axis.
+	 * 0 radians == pointing along the Z axis.
 	 */
-	public float theta = 0.0f;
+	private float theta = (float)(Math.PI * 0.5);
 	
 	/**
-	 * The ZY displacement of the camera from 0 degrees in radians.
+	 * The displacement of the camera in radians along the XY plane.
+	 * 0 radians == pointing along the X axis.
 	 */
-	public float chi = 0.0f;
+	private float phi = 0.0f;
 	
 	/**
 	 * Create a camera at {0,0,5} with rotation values {0,0,0}.
@@ -83,12 +85,52 @@ public class Camera {
 	}
 
 	/**
-	 * Updates the camera's position vector based on the radius, theta and chi values.
+	 * Updates the camera's position vector based on the radius, Theta and Chi values.
+	 * I swapped the Z and Y equations with each other to work in my coordinate system.... TODO might need to change this back...
+	 * @see "https://en.wikipedia.org/wiki/Spherical_coordinate_system"
 	 */
 	public void updatePosition() {
-		this.worldPosition.x = (float)Math.sin(theta) * radius;
-		this.worldPosition.z = (float)Math.cos(theta) * radius; //TODO something isn't quite right here...
-		this.worldPosition.y = (float)Math.cos(chi) * radius;
+		this.worldPosition.x = radius * ((float) Math.cos(phi)) * ((float) Math.sin(theta));
+		this.worldPosition.y = radius * ((float) Math.cos(theta));
+		this.worldPosition.z = radius * ((float) Math.sin(phi)) * ((float) Math.sin(theta));
+		/*
+		//old code
+		this.worldPosition.y = radius * ((float) Math.sin(phi)) * ((float) Math.sin(theta));
+		this.worldPosition.z = radius * ((float) Math.cos(theta));
+		*/
+		System.out.println("camera position: ["+worldPosition.x+" "+worldPosition.y+" "+worldPosition.z+"]");
+	}
+	
+	/**
+	 * Updates the Phi angle used by the camera to determine its position.
+	 * The Phi angle ranges between 0 and 2 * PI radians.
+	 * The angle will wrap around to remain in these bounds if 'angle' causes Phi to exceed the bounds.
+	 * @param angle
+	 *  The amount that will be added to the angle IN DEGREES.
+	 *  Can be negetive.
+	 */
+	public void addToPhi(float angle) {
+		float amount = (float)Math.toRadians(angle);
+		phi += amount;
+		if(phi >= (2.0 * Math.PI)) phi = 0.0f;
+		if(phi < 0.0f) phi = (float)(1.999 * Math.PI); //according to the wiki page, 2PI value is invalid
+		System.out.println("Phi updated: "+phi);
+	}
+	
+	/**
+	 * Updates the Theta angle used by the camera to determine its position.
+	 * The Theta angle can range between 0 and PI radians. I am reducing this range to between 0 and PI * 0.5.
+	 * The angle will be clipped to remain in these bounds if 'angle' causes Theta to exceed the bounds.
+	 * @param angle
+	 *  The amount that will be added to the angle IN DEGREES.
+	 *  Can be negetive.
+	 */
+	public void addToTheta(float angle) {
+		float amount = (float)Math.toRadians(angle);
+		theta += amount;
+		if(theta > 0.5 * Math.PI) theta = (float)(0.5 * Math.PI);
+		if(theta < 0.0f) theta = 0.01f;
+		System.out.println("Theta updated: "+theta);
 	}
 	
 	
