67f821eca9897956549fb6e2b2c3a3d33da6dfbc
Benjamin Sanson <bsanson90@gmail.com>
2022-02-04 21:47:11 +1300

    fragment can use either a colour or a texture

--- src/connect3DResources/fragment.fs
+++ src/connect3DResources/fragment.fs
@@ -4,8 +4,17 @@ in vec2 exTextureCoordinate;
 out vec4 fragmentColor;
 
 uniform sampler2D texture_sampler; //sampler2D holds the texture unit on the GPU that should be queried for texture color data.
+uniform vec3 color; //the color to use if the use color flag has been set.
+uniform int useColor; //A flag to indicate whether we should use the texture sampler or the color value. +ve value = use the color.
 
 void main()
 {
-	fragmentColor = texture(texture_sampler, exTextureCoordinate);
+	if( useColor == 1 )
+	{
+		fragmentColor = vec4(color, 1);
+	} 
+	else
+	{
+		fragmentColor = texture(texture_sampler, exTextureCoordinate);
+	}
 }
