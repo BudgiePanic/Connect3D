ca2f4dd21c83ae3e1dcd6f290115a2bfb24dd047
Benjamin Sanson <bsanson90@gmail.com>
2022-02-04 21:46:46 +1300

    Pieces now supply RGB float vectors

--- src/connect3DCore/Piece.java
+++ src/connect3DCore/Piece.java
@@ -1,6 +1,8 @@
 package connect3DCore;
 
 import java.awt.Color;
+
+import org.joml.Vector3f;
 /**
  * Symbolic constants to refer to the different pieces on the game board.
  * Each player places a different piece.
@@ -14,6 +16,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return Color.RED; }
+
+		@Override
+		public Vector3f colorVector() {
+			return new Vector3f(1.0f, 0.0f, 0.0f);
+		}
 	},
 	BLUE {
 		@Override
@@ -21,6 +28,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return Color.BLUE; }
+
+		@Override
+		public Vector3f colorVector() {
+			return new Vector3f(0.0f, 0.0f, 1.0f);
+		}
 	},	
 	GREEN {
 		@Override
@@ -28,6 +40,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return Color.GREEN; }
+
+		@Override
+		public Vector3f colorVector() {
+			return new Vector3f(0.0f, 1.0f, 0.0f);
+		}
 	},
 	YELLOW {
 		@Override
@@ -35,6 +52,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return Color.YELLOW; }
+
+		@Override
+		public Vector3f colorVector() {
+			return new Vector3f(1.0f, 1.0f, 0.0f);
+		}
 	},
 	ORANGE {
 		@Override
@@ -42,6 +64,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return Color.ORANGE;	}
+
+		@Override
+		public Vector3f colorVector() {
+			return new Vector3f(1.0f, 0.3f, 0.0f);
+		}
 	},
 	PURPLE {
 		@Override
@@ -49,6 +76,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return new Color(103, 58, 183); }
+
+		@Override
+		public Vector3f colorVector() {
+			return new Vector3f(0.4f, 0.0f, 1.0f);
+		}
 	},	
 	WHITE {
 		@Override
@@ -56,6 +88,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return Color.WHITE; }
+
+		@Override
+		public Vector3f colorVector() {
+			return new Vector3f(1.0f, 1.0f, 1.0f);
+		}
 	},
 	EMPTY {
 		@Override
@@ -63,6 +100,11 @@ public enum Piece {
 
 		@Override
 		public Color color() { return null; }
+
+		@Override
+		public Vector3f colorVector() {
+			return null;
+		}
 	};
 	
 	/**
@@ -78,4 +120,11 @@ public enum Piece {
 	 *  The piece's color.
 	 */
 	public abstract Color color();
+
+	/**
+	 * Returns a Vector3f representation of this piece's color.
+	 * @return
+	 * The color of this piece.
+	 */
+	public abstract Vector3f colorVector();
 }
