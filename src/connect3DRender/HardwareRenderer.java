1ad4f5e897a183c6b9d602f4b21aebfa11e3ddb0
Benjamin Sanson <bsanson90@gmail.com>
2022-02-27 14:18:56 +1300

    enable back face culling optimization

--- src/connect3DRender/HardwareRenderer.java
+++ src/connect3DRender/HardwareRenderer.java
@@ -235,6 +235,8 @@ public final class HardwareRenderer implements Renderer {
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_BACK);
 		
 		glfwSetFramebufferSizeCallback(a_window, (window, width, height)->{
 			this.WIDTH = width; this.HEIGHT = height;
